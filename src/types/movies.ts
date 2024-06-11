5e246b83ddb6313e84a39f7a56d7e5e3962857a2
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-11 16:10:29 +0900

    feat: 영화 상세정보 타입 정의

--- src/types/movies.ts
+++ src/types/movies.ts
@@ -1,7 +1,26 @@
 export interface Movie {
   id: number;
   title: string;
+  poster_path: string;
+  vote_average: number;
+}
+
+interface MovieDetailStandard {
+  id: number;
+  title: string;
   overview: string;
   poster_path: string;
   vote_average: number;
+}
+
+interface GenreResponse {
+  id: number;
+  name: string;
+}
+
+export interface MovieDetailResponse extends MovieDetailStandard {
+  genres: GenreResponse[];
+}
+export interface MovieDetail extends MovieDetailStandard {
+  genres: string[];
 }
