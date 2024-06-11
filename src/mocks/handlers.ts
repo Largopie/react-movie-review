cd64e9065bfa66cf2a43bf8eca48c089586e3877
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-11 16:10:48 +0900

    feat: 영화 상세정보 msw handler 설정

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,20 @@
 import { http, HttpResponse } from 'msw';
+import { BASE_URL, END_POINT } from '../api/endpoints';
+import { Movie, MovieDetailResponse } from '../types/movies';
+
 import moviesData from './movies.json';
-import { END_POINT } from '../api/endpoints';
-import { Movie } from '../types/movies';
+import movieData from './movie.json';
 
 export const handlers = [
   http.get(`${END_POINT.popularMovies}`, () => {
     const movies = moviesData as Movie[];
 
     return HttpResponse.json({ results: movies });
+  }),
+
+  http.get(`${BASE_URL}/:movieId`, () => {
+    const movie = movieData as MovieDetailResponse;
+
+    return HttpResponse.json({ ...movie, genres: ['애니메이션', '액션', '가족', '코미디', '판타지'] });
   }),
 ];
