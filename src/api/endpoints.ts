c75ce078dd00359d08a46e57182ca3db5665337b
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-11 15:02:05 +0900

    chore: TMDB image base url 설정

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,4 +1,5 @@
 export const BASE_URL = 'http://example.com';
+export const POSTER_URL = 'https://image.tmdb.org/t/p/original';
 
 export const END_POINT = {
   popularMovies: `${BASE_URL}/movie/popular`,
