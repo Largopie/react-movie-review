b1a9cba59c6e536b73d3ea605a40817987c3be5e
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-11 14:29:51 +0900

    feat(useMovies): 로딩 상태 및 다음 페이지 렌더링 로직 구현

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -4,20 +4,39 @@ import { fetchPopularMovies } from '../api/movies';
 
 const useMovies = () => {
   const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
   const [movies, setMovies] = useState<Movie[]>([]);
 
-  useEffect(() => {
-    const getMovies = async () => {
+  const getMovies = async (page: number) => {
+    try {
+      setIsLoading(true);
       const movies = await fetchPopularMovies(page);
 
       setMovies(movies);
       setPage((prevPage) => prevPage + 1);
-    };
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    getMovies();
+  useEffect(() => {
+    getMovies(page);
   }, []);
 
-  return movies;
+  const fetchNextPage = async () => {
+    const nextPage = page + 1;
+
+    try {
+      await getMovies(nextPage);
+      setPage(nextPage);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { movies, isLoading, fetchNextPage };
 };
 
 export default useMovies;
