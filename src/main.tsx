fc1d1dc06d6016401b71b8c736f2d8c62bdd5345
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-11 15:11:48 +0900

    feat: msw 브라우저에서 동작 가능하도록 구현

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,23 @@ import App from './App.tsx';
 import { Global } from '@emotion/react';
 import { reset } from './reset.ts';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Global styles={reset} />
-    <App />
-  </React.StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <Global styles={reset} />
+      <App />
+    </React.StrictMode>,
+  );
+});
