e35d84fc0bd603970b0fda813ac049eba48b4c02
Largopie <106071687+Largopie@users.noreply.github.com>
2024-06-11 15:02:21 +0900

    feat: MovieItem 스토리북 작성 및 컴포넌트 구현

---
+++ src/components/MovieItem/index.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import { Movie } from '../../types/movies';
+
+const MovieItem = ({ poster_path, title, vote_average }: Movie) => {
+  return (
+    <MovieItemContainer>
+      <Poster src={poster_path} />
+      <Title>{title}</Title>
+      <Average>{vote_average.toFixed(1)}</Average>
+    </MovieItemContainer>
+  );
+};
+
+export default MovieItem;
+
+const MovieItemContainer = styled.li`
+  width: 182px;
+  height: 359px;
+`;
+
+const Poster = styled.img`
+  width: 100%;
+  height: 273px;
+
+  border-radius: 15px;
+`;
+
+const Title = styled.h2`
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 27px;
+  color: #fff;
+`;
+
+const Average = styled.span`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 27px;
+  color: #fff;
+`;
